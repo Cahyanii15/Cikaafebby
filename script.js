634ced909db02db83aa0dc1d4acf739729304dd8
Cahyanii15 <febbychyaa@gmail.com>
2025-10-30 11:14:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 cost tombolSapa = document.getElementByld('sapaButton')
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function(){
-// Jalankan kode ini ketika tombol di-klik
-alert('Halo! Terima kasih sudah berkunjung!');
+   // Jalankan kode ini ketika tombol di-klik
+   alert('Halo! Terima kasih sudah berkunjung!');
 });
